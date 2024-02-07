ca50cad65282dc364d43e55a42200fce502a5979
ERICKXX17 <141387722+ERICKXX17@users.noreply.github.com>
2024-02-06 20:56:36 -0600

    Create user.js

---
+++ user.js
@@ -0,0 +1,10 @@
+import { signOut,auth } from "./firebase.js";
+
+cerrar.addEventListener('click',(e)=>{
+    signOut(auth).then(()=>{  
+      alert("Sesion cerrada");
+      window.location.href='./index.html';
+    }).catch((e)=>{
+      alert("Error al cerrar sesion: "+e);
+    });
+  })
